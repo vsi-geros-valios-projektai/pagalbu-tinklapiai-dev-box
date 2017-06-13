c0e4a3f76c866d31e7aed645b65c41961da42845
Paulius Mačernis <sugalvojau@gmail.com>
2017-06-12 21:40:33 -0400

    Updating. Server has changed a lot, name of projects changed, etc.

--- _install/puphpet/files/exec-once/config-nginx.sh
+++ _install/puphpet/files/exec-once/config-nginx.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
-cp /vagrant/_install/nginx/pagalbadaiktais.dev.conf /etc/nginx/sites-enabled/pagalbadaiktais.dev.conf
-cp /vagrant/_install/nginx/pagalbadarbais.dev.conf /etc/nginx/sites-enabled/pagalbadarbais.dev.conf
-cp /vagrant/_install/nginx/admin.pagalbadaiktais.dev.conf /etc/nginx/sites-enabled/admin.pagalbadaiktais.dev.conf
+cp /vagrant/_install/nginx/aukokdaiktus.dev.conf /etc/nginx/sites-enabled/aukokdaiktus.dev.conf
+cp /vagrant/_install/nginx/aukoklaika.dev.conf /etc/nginx/sites-enabled/aukoklaika.dev.conf
+cp /vagrant/_install/nginx/admin.aukokdaiktus.dev.conf /etc/nginx/sites-enabled/admin.aukokdaiktus.dev.conf
 service nginx restart
 
-rm -f /home/pagalbadaiktais.dev/httpdocs/.gitignore
-git clone https://github.com/vsi-geros-valios-projektai/pagalbadaiktais.lt.git /home/pagalbadaiktais.dev/httpdocs
+#rm -f /home/aukokdaiktus.dev/httpdocs/.gitignore
+#git clone ssh://git@mano.gerasfabrikas.lt/source/aukokdaiktus.lt.git /home/aukokdaiktus.dev/httpdocs
 
-rm -f /home/pagalbadarbais.dev/httpdocs/.gitignore
-git clone https://github.com/vsi-geros-valios-projektai/pagalbadarbais.lt.git /home/pagalbadarbais.dev/httpdocs
+#rm -f /home/aukoklaika.dev/httpdocs/.gitignore
+#git clone ssh://git@mano.gerasfabrikas.lt/source/aukoklaika.lt.git /home/aukoklaika.dev/httpdocs
 
-rm -f /home/admin.pagalbadaiktais.dev/httpdocs/.gitignore
-git clone https://github.com/vsi-geros-valios-projektai/admin.pagalbadaiktais.lt.git /home/admin.pagalbadaiktais.dev/httpdocs
+#rm -f /home/admin.aukokdaiktus.dev/httpdocs/.gitignore
+#git clone ssh://git@mano.gerasfabrikas.lt/source/admin.aukokdaiktus.lt.git /home/admin.aukokdaiktus.dev/httpdocs
 
-wget  -O /home/_install/db/dev-backup-latest.sql "http://admin.pagalbadaiktais.lt/dev/backup/db/dev-backup-latest.sql"
-mysql --user=pdarbais --password=pdarbais pdarbais < "/home/_install/db/dev-backup-latest.sql"
+#wget  -O /home/_install/db/dev-backup-latest.sql "http://admin.aukokdaiktus.lt/dev/backup/db/dev-backup-latest.sql"
+#mysql --user=pdarbais --password=pdarbais pdarbais < "/home/_install/db/dev-backup-latest.sql"
